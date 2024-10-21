a61b5dfaa365d00f501c351331194a86f3d1a351
lee_sumin <leesm.sumin@gmail.com>
2024-10-22 00:44:28 +0900

    feat: useProducts로 파일명 변경 및 기능 추가

---
+++ src/refactoring/hooks/useProducts.ts
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Product } from "../../types.ts";
+
+export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts((prevProducts) => prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p)));
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts([...products, newProduct]);
+  };
+
+  return { products, updateProduct, addProduct };
+};
