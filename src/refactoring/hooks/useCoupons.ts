481f13fac9c43dfe5ab7462f00ea9a41e5996932
lee_sumin <leesm.sumin@gmail.com>
2024-10-22 00:45:07 +0900

    feat: useCoupons로 파일명 변경 및 기능 추가

---
+++ src/refactoring/hooks/useCoupons.ts
@@ -0,0 +1,12 @@
+import { Coupon } from "../../types.ts";
+import { useState } from "react";
+
+export const useCoupons = (initialCoupons: Coupon[]) => {
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  const addCoupon = (coupon: Coupon) => {
+    setCoupons([...coupons, coupon]);
+  };
+
+  return { coupons, addCoupon };
+};
