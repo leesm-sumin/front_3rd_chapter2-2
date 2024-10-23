49b2286d812f5c83647a53b71453170c596caa56
lee_sumin <leesm.sumin@gmail.com>
2024-10-23 20:54:06 +0900

    refactor: 파라미터명 변경

--- src/refactoring/hooks/useProducts.ts
+++ src/refactoring/hooks/useProducts.ts
@@ -5,7 +5,9 @@ export const useProducts = (initialProducts: Product[]) => {
   const [products, setProducts] = useState<Product[]>(initialProducts);
 
   const updateProduct = (updatedProduct: Product) => {
-    setProducts((prevProducts) => prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p)));
+    setProducts((prevProducts) =>
+      prevProducts.map((product) => (product.id === updatedProduct.id ? updatedProduct : product)),
+    );
   };
 
   const addProduct = (newProduct: Product) => {
